fce6f31983d0738f0ea79457200eaa356aff4d46
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-05-28 22:35:03 -0300

    Update main.hs

--- trab04/main.hs
+++ trab04/main.hs
@@ -1,6 +1,6 @@
 import Data.List (intercalate)
 
-data Frame = Strike | Spare Int | Open Int Int | Extra Int deriving (Show)
+data Frame = Strike | Spare Int | Open Int Int | SingleStrike | SingleThrow Int deriving (Show)
 
 toFrames :: [Int] -> [Frame]
 toFrames pins = toFrame pins 1
@@ -13,19 +13,23 @@ toFrame (x:y:xs) n
     | otherwise   = Open x y : toFrame xs (n + 1)
       
 -- Converte o último Frame em algo entendivel pelo algoritmo
-finalFrame :: [Int] -> [Frame]
+finalFrame:: [Int] -> [Frame] 
 finalFrame [x,y,z]
-    | x + y == 10 = [Spare x, Extra z]
-    | otherwise = [Extra x, Extra y, Extra z]
-finalFrame [x,y] = [Open x y]
-
+    | [x,y,z] == [10,10,10] = [SingleStrike, SingleStrike, SingleStrike]
+    | [x,y,z] == [10,10, z] = [SingleStrike, SingleStrike, SingleThrow z]
+    | x == 10 && y + z == 10 = [SingleStrike, Spare y]
+    | [x,y,z] == [10, y,10] = [SingleStrike, SingleThrow y, SingleStrike]
+    | [x,y,z] == [10, y, z] = [SingleStrike, SingleThrow y, SingleThrow z]
+    | [x,y,z] == [x, y, 10] = [Spare x, SingleStrike]
+    | [x,y,z] == [x, y, z] = [Spare x, SingleThrow z]
+finalFrame [x,y] = [SingleThrow x, SingleThrow y]
+    
 showFrame :: Frame -> String
 showFrame Strike = "X _"
 showFrame (Spare x) = show x ++ " /"
 showFrame (Open x y) = show x ++ " " ++ show y
-showFrame (Extra x)
-    | x == 10 = "X"
-    | otherwise = show x
+showFrame SingleStrike = "X"
+showFrame (SingleThrow x) = show x
 
 printFrames :: [Frame] -> String
 printFrames frame = printFrame 1 (map showFrame frame)
@@ -52,31 +56,7 @@ main :: IO ()
 main = do
     input <- getLine
     let numbers = map read $ words input :: [Int]
-    -- let numbers = [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]
-    -- let numbers = [10,10,10,10,10,10,10,10,10,10,10,10]
     let text = printFrames $ toFrames numbers
     let value = show $ score numbers 1
     let answer = text ++ " | " ++ value
     putStrLn answer
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
